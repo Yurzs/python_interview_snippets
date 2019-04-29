48968fba95b36e67f91fd67111a5bd5dd181f3ce
Yurzs <yurzs@icloud.com>
2019-04-29 20:49:53 +0300

    find_substring

--- snippets/find_substring.py
+++ snippets/find_substring.py
@@ -4,19 +4,12 @@ def find_substing(s: str, pattern: str) -> list:
     """
     Find substring in string
     :param s: String
-    :param pattern: Pattern to match
-    :return: list of match [start, end]
+    :param pattern: Pattern
+    :return: list of [start, finish] match indexes
     """
     match_storage = []
-    match_counter = 0
-    pattern_index = 0
     for n, c in enumerate(s):
-        if pattern_index > 0 and c == pattern[0]:
-            match_counter = 1
-            pattern_index = 1
-        else:
-            pattern_index +=1
-            match_counter +=1
-        if match_counter == len(pattern):
-            match_storage.append([n-len(pattern), n])
+        if c == pattern[0]:
+            if s[n:n+len(pattern)] == pattern:
+                match_storage.append([n, n + len(pattern)])
     return match_storage
